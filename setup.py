ca5959d9a7c83655b9f44978f34a5524213fd585
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2019-12-17 16:25:53 +0530

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='qbosdk',
+    version='0.1.0',
+    author='Shwetabh Kumar',
+    author_email='shwetabh.kumar@fyle.in',
+    description='Python SDK for accessing Quickbooks Online APIs',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['quickbooks-online', 'quickbooks', 'fyle', 'api', 'python', 'sdk'],
+    url='https://github.com/fylein/qbo-sdk-py',
+    packages=setuptools.find_packages(),
+    install_requires=['requests==2.22.0', 'future==0.18.2'],
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)
