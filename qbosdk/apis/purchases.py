d09937b00cbf2ae69a9a1dcb44e21011fa6e52fc
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2019-12-18 22:16:27 +0530

    Delete Journal Entries and Purchases By Id

--- qbosdk/apis/purchases.py
+++ qbosdk/apis/purchases.py
@@ -10,6 +10,7 @@ class Purchases(ApiBase):
 
     GET_PURCHASES = '/query?query=select * from Purchase STARTPOSITION {0} MAXRESULTS 1000'
     POST_PURCHASE = '/purchase?minorversion=38'
+    DELETE_PURCHASE = '/purchase?operation=delete'
 
     def get(self):
         """
@@ -25,3 +26,15 @@ class Purchases(ApiBase):
         :return:
         """
         return self._post_request(data, Purchases.POST_PURCHASE)
+
+    def delete(self, purchase_id: str):
+        """
+        Delete Purchase (check, etc) from Quickbooks Online
+        :param purchase_id: Journal Entry Id to remove
+        :return: Dict response
+        """
+        data = {
+            'Id': purchase_id,
+            'SyncToken': '1'
+        }
+        return self._post_request(data, Purchases.DELETE_PURCHASE)
