489db6960aa5d0493836479722df32dba77d127a
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2021-02-23 12:25:38 +0530

    Support for POST of Employees and Vendors in QBO SDK

--- qbosdk/apis/employees.py
+++ qbosdk/apis/employees.py
@@ -1,6 +1,8 @@
 """
 Quickbooks Online employees
 """
+from typing import Dict
+
 from .api_base import ApiBase
 
 
@@ -8,6 +10,7 @@ class Employees(ApiBase):
     """Class for Categories APIs."""
 
     GET_EMPLOYEES = '/query?query=select * from Employee STARTPOSITION {0} MAXRESULTS 1000'
+    POST_EMPLOYEE = '/employee?minorversion=38'
 
     def get(self):
         """Get a list of the existing Employees in the Organization.
@@ -16,3 +19,11 @@ class Employees(ApiBase):
             List with dicts in Employees schema.
         """
         return self._query_get_all('Employee', Employees.GET_EMPLOYEES)
+
+    def post(self, data: Dict):
+        """
+        Post Employee to Quickbooks Online
+        :param data: Dict in Employee schema
+        :return:
+        """
+        return self._post_request(data, Employees.POST_EMPLOYEE)
--- qbosdk/apis/vendors.py
+++ qbosdk/apis/vendors.py
@@ -1,6 +1,8 @@
 """
 Quickbooks Online vendors
 """
+from typing import Dict
+
 from .api_base import ApiBase
 
 
@@ -8,6 +10,7 @@ class Vendors(ApiBase):
     """Class for Categories APIs."""
 
     GET_VENDORS = '/query?query=select * from Vendor STARTPOSITION {0} MAXRESULTS 1000'
+    POST_VENDOR = '/vendor?minorversion=38'
 
     def get(self):
         """Get a list of the existing Vendors in the Organization.
@@ -16,3 +19,11 @@ class Vendors(ApiBase):
             List with dicts in Vendors schema.
         """
         return self._query_get_all('Vendor', Vendors.GET_VENDORS)
+
+    def post(self, data: Dict):
+        """
+        Post Vendor to Quickbooks Online
+        :param data: Dict in Vendor schema
+        :return:
+        """
+        return self._post_request(data, Vendors.POST_VENDOR)
