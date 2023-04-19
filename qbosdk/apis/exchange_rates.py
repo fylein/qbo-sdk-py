fe28a9071d8ef2398337ca2efb0dc2b1f93ea33b
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2023-04-19 21:12:39 +0530

    Get Exchange Rate by Source (#19)
    * Get Exchange Rate by Source
    
    * Remove trailing new lines

--- qbosdk/apis/exchange_rates.py
+++ qbosdk/apis/exchange_rates.py
@@ -10,6 +10,7 @@ class ExchangeRates(ApiBase):
 
     GET_EXCHANGE_RATES = "/query?query=select * from ExchangeRate where AsOfDate = '{0}' STARTPOSITION " \
                          "{1} MAXRESULTS 1000"
+    GET_EXCHANGE_RATES_BY_SOURCE = "/exchangerate?sourcecurrencycode={0}"
 
     def get(self, as_of_date: str = None):
         """
@@ -22,3 +23,12 @@ class ExchangeRates(ApiBase):
 
         ExchangeRates.GET_EXCHANGE_RATES = ExchangeRates.GET_EXCHANGE_RATES.format(as_of_date, '{0}')
         return self._query_get_all('ExchangeRate', ExchangeRates.GET_EXCHANGE_RATES)
+
+    def get_by_source(self, source_currency_code: str):
+        """
+        Get all the exchange rates
+        :param as_of_date: date to get rates for (1 day prior if left empty)
+        :return: List of Dicts for exchange rates
+        """
+        return self._get_request(
+            'ExchangeRate', self.GET_EXCHANGE_RATES_BY_SOURCE.format(source_currency_code))
