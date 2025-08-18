23ea68c95069a21a75b042c46419d825bbd8b698
ruuushhh <rushikeshtayade2001@gmail.com>
2025-08-18 16:25:01 +0530

    fix: vul CVE-2025-50817

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='qbosdk',
-    version='1.4.0',
+    version='1.4.1',
     author='Shwetabh Kumar',
     author_email='shwetabh.kumar@fyle.in',
     description='Python SDK for accessing Quickbooks Online APIs',
@@ -18,7 +18,7 @@ setuptools.setup(
     keywords=['quickbooks-online', 'quickbooks', 'fyle', 'api', 'python', 'sdk'],
     url='https://github.com/fylein/qbo-sdk-py',
     packages=setuptools.find_packages(),
-    install_requires=['requests>=2.25.0', 'future==0.18.2'],
+    install_requires=['requests>=2.25.0'],
     classifiers=[
         'Topic :: Internet :: WWW/HTTP',
         'Intended Audience :: Developers',
